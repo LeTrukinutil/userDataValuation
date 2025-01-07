e9cfb11e025403101efcd7c65db0693ecb7aa318
Amine Derbel <derbel.amine.49@gmail.com>
2025-01-07 14:27:59 +0100

    ajout breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanySearchController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('research');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/search', [CompanySearchController::class, 'search']);
+    Route::get('/', function(){
+        return view('search');
+    });
 });
 
-Route::get('/recherche', [CompanySearchController::class, 'search'])->name('search');
+require __DIR__.'/auth.php';
